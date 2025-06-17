c44f6936587782762c462cd8b35c5953590eb569
unknown <juhbuscariollo026@mgail.com>
2025-06-17 13:48:21 -0300

    primeiro commit

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+let nome = 0;
+let nomes = [];
+let endereco = 0; 
+let enderecos = [];
+let distancia = 0;
+let distancias = [];
+let entrega = 0;
+let urgente = 0;
+let valorFinal = 0;
+const valor = 2;
+
+
