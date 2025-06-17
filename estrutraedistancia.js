7837a8e54d9716b6a8749ab7554d959cc93aacb1
unknown <brunonavarrodavi@gmail.com>
2025-06-17 15:17:05 -0300

    estrutura com distância

--- estrutraedistancia.js
+++ estrutraedistancia.js
@@ -6,6 +6,7 @@ let nomeArquivo = "Dados.txt";
 let nomes = [];
 let enderecos = [];
 let distancias = [];
+let valoresKm = [];
 
 let nome = 0;
 let endereco = 0;
@@ -13,7 +14,7 @@ let distancia = 0;
 let entrega = 0;
 let urgente = 0;
 let valorFinal = 0;
-const valor = 2;
+let valorKm = 0;
 
 let i = 0;
 let parar = 0;
@@ -46,6 +47,36 @@ do {
 
 for (let  i= 0;  i< distancias.length; i++) {
     
-    console.log(`${distancias[i]}`);
+//    console.log(`${distancias[i]}`);
+    
+}
+
+do {
+    
+    //repetir até conseguir o valor da variável nome correto
+    do {
+        valor = promptSync(`Insira o valor da entrega:`);
+    
+        //verificação da variável nome
+        if (isNaN(valor)) {
+            console.log(`ERRO`);
+    
+        } else {
+            valorKm[i] = valor;
+            conteudo += +valor[i]+" - ";
+        }
+        
+    } while (isNaN(valor));
+
+
+
+    parar = promptSync(`Caso deseje continuar digite "sim" ou "s"!`);
+    i++;
+
+} while (parar == "sim" || parar == "s");
+
+for (let  i= 0;  i< valores.length; i++) {
+    
+    console.log(`${valores[i]} ${distancias[i]}`);
     
 }
