8268f8dec0c0d022d5dff77c2ffc9375341936ef
unknown <nandinhaa1280@gmail.com>
2025-06-17 13:46:21 -0300

    inicio

---
+++ S1_R9-AT1_LOPAL.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const promptSync = require("prompt-sync")();
+const path = require("path");
+
+let nomes = [];
+let enderecos = [];
+let distancias = [];
+
+let nome = 0;
+let endereco = 0;
+let distancia = 0;
+let entrega = 0;
+let urgente = 0;
+let valorFinal = 0;
+const valor = 2;
+
+do {
+    
+} while (condition);
