f60f3b8f5e0fc4d509394f022914c8d1c9be7983
unknown <juhbuscariollo026@mgail.com>
2025-06-17 15:37:24 -0300

    calculo e urgencia da entrega

--- calculadora.js
+++ calculadora.js
@@ -4,28 +4,23 @@ let endereco = 0;
 let enderecos = [];
 let distancia = 0;
 let distancias = [];
-let entregas = [];
 let comum = 0;
-let urgente = 0;
+let urgente = 'não';
 let valorFinal = 0;
 let valor = 0;
 
 
 
+entrega = valor*distancia;
 
-let tipoEntrega 
-while (true)
-   {
-    promptSync('Tipo de entrega (normal/urgente): ');
+   let tipoEntrega= promptSync('Entrega urgente? Confirme com "s": ');
 
-    if (tipoEntrega() !='normal' && tipoEntrega() != 'urgente') {
-        ('Tipo inválido! Digite apenas "normal" ou "urgente".');
+    if (tipoEntrega == 's') {
+        entrega = entrega*1.2;
+        urgente = 'sim';
+    } 
 
-    } else {
 
-        if () {
-            
-        }
-    }
-}
+
+    
 let conteudo = `Nome: ${nomes}, Endereço: ${enderecos}, Distância: ${distancias}, Urgência: ${urgente}, Valor: ${valorFinal}`;
