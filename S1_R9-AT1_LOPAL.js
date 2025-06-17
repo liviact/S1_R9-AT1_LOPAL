5e42dc8e6ca3865d0d4ff94d6f705ef3c4cca521
unknown <nandinhaa1280@gmail.com>
2025-06-17 14:38:36 -0300

    variaveis, estrutura do dowhile e verificação de cada informação

--- S1_R9-AT1_LOPAL.js
+++ S1_R9-AT1_LOPAL.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const promptSync = require("prompt-sync")();
 const path = require("path");
+let nomeArquivo = "Dados.txt";
 
 let nomes = [];
 let enderecos = [];
@@ -14,6 +15,37 @@ let urgente = 0;
 let valorFinal = 0;
 const valor = 2;
 
+let i = 0;
+let parar = 0;
+let conteudo = "NOME - ENDEREÇO - DISTÂNCIA - VALOR P/KM - TIPO DE ENTREGA - CUSTO TOTAL";
+fs.writeFileSync(nomeArquivo, conteudo, "utf8");
+
 do {
     
-} while (condition);
+    //repetir até conseguir o valor da variável nome correto
+    do {
+        nome = promptSync(`Insira seu nome:`);
+    
+        //verificação da variável nome
+        if (!(isNaN(nome))) {
+            console.log(`ERRO`);
+    
+        } else {
+            nomes[i] = nome;
+            conteudo += "\n"+nomes[i]+" - ";
+        }
+        
+    } while (nome = !(isNaN(nome)));
+
+
+
+    parar = promptSync(`Caso deseje continuar digite "sim" ou "s"!`);
+    i++;
+
+} while (parar == "sim" || parar == "s");
+
+for (let  i= 0;  i< nomes.length; i++) {
+    
+    console.log(`${nomes[i]}`);
+    
+}
